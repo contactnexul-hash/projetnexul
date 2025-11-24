9ce11a67edf20ba8f0642ebe1390b85a1acf0c77
AISSANI <contact.nexul@gmail.com>
2025-11-24 01:06:18 +0100

    Ajout layout education

---
+++ app/poles/education/layout.tsx
@@ -0,0 +1,1 @@
+export default function EducationLayout({ children }: { children: React.ReactNode }) { return <div style={{ padding: "2rem", background: "#080808", color: "#fff" }}>{children}</div>; }

2334395c7509fb0b4182881586cbbc3807022d33
AISSANI <contact.nexul@gmail.com>
2025-11-30 16:31:43 +0100

    Corrige les imports et ajoute framer-motion

--- app/poles/education/actions/page.tsx
+++ app/poles/education/actions/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Something } from './actions';
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { submitVolunteer } from "./actions/actions";

c5055191ce69ae62a05fb290ee78534839f8d813
AISSANI <contact.nexul@gmail.com>
2025-12-01 20:47:23 +0100

    Fix Chart.js scale registration automatically on all charts

--- components/MonChart.js
+++ components/MonChart.js
@@ -1,5 +1,7 @@
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 import {
   Chart as ChartJS,
   CategoryScale,

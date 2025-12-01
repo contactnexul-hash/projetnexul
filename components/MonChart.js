de943498461c9ab732c7f0944555ce3d94bf1f7f
AISSANI <contact.nexul@gmail.com>
2025-12-01 21:14:58 +0100

    Fix Chart.js dynamic import SSR for all charts including subfolders

--- components/MonChart.js
+++ components/MonChart.js
@@ -1,6 +1,7 @@
 import "../lib/charts";
 import "../lib/charts";
 import "../lib/charts";
+import "../lib/charts";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";

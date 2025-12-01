16dfa62d89cdcd67702f0848872098f7110d7584
AISSANI <contact.nexul@gmail.com>
2025-12-01 21:18:45 +0100

    Convert all charts to client-side dynamic components

---
+++ components/MonChartClient.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import '../lib/charts';
+
+export default function MonChartClient(props) {
+  const [ChartComponent, setChartComponent] = useState(null);
+  useEffect(() => {
+    const DynamicChart = dynamic(() => import("react-chartjs-2").then(mod => mod.MonChart), { ssr: false });
+    setChartComponent(() => DynamicChart);
+  }, []);
+  if (!ChartComponent) return null;
+  return <ChartComponent {...props} />;
+}

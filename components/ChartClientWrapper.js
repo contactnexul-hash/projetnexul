22300adb24c3c5c8815144a0fdd1af828db4aeb2
AISSANI <contact.nexul@gmail.com>
2025-12-01 22:33:33 +0100

    Fix ChartClientWrapper.js: register CategoryScale client-side

--- components/ChartClientWrapper.js
+++ components/ChartClientWrapper.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-import '../lib/charts';
 
 export default function ChartClientWrapper({ type, data, options }) {
   const [ChartComponent, setChartComponent] = useState(null);
 
   useEffect(() => {
+    import('../lib/charts');
+
     if (type === 'Line') {
       const Line = dynamic(() => import("react-chartjs-2").then(mod => mod.Line), { ssr: false });
       setChartComponent(() => Line);
